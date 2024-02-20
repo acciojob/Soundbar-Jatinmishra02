4c7b4838306a26dbe989a1ca65e407ecbf4353dc
Jatin Mishra <104919571+Jatinmishra02@users.noreply.github.com>
2024-02-20 20:50:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function playSound(soundFile) {
+  const audio = new Audio(`sounds/${soundFile}`);
+  audio.play();
+}
+
+function stopSound() {
+  const audioElements = document.querySelectorAll('audio');
+  audioElements.forEach(audio => audio.pause());
+}
